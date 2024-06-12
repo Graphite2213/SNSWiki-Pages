7262a7f35967a0a4f179ed474f0116b9519d8457
Graphite2213 <88732099+Graphite2213@users.noreply.github.com>
2024-06-12 20:51:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 import glob
+import json
+
 filenamesEn = glob.glob("./en/*.html")
 filenamesSr = glob.glob("./rs/*.html")
 
-print(filenamesEn)
-print(filenamesSr)
+with open("./en/en-meta.json", 'r') as file:
+    metaEn = json.load(file)
+
+with open("./rs/rs-meta.json", 'r') as file:
+    metaRs = json.load(file)
+
+allFilesEn = []
+allFilesRs = []
+
+for x in filenamesEn:
+    allFilesEn.append(x.split("/")[-1]);
+
+
+for x in filenamesSr:
+    allFilesRs.append(x.split("/")[-1]);
+
+metaEn["pages"] = allFilesEn;
+metaRs["pages"] = allFilesRs;
+
+with open("./en/en-meta.json", 'w') as file:
+    json.dump(metaEn, file, indent=4)
+
+with open("./rs/rs-meta.json", 'w') as file:
+    json.dump(metaRs, file, indent=4)
