5c4744c51ecd7752ae6cdf3e3e0808ad52e7c7c6
Graphite <88732099+Graphite2213@users.noreply.github.com>
2024-11-24 12:07:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,43 @@
 import glob
 import json
+import numpy as np
 
-filenamesEn = glob.glob("./en/*/")
-filenamesSr = glob.glob("./rs/*/")
+def main():
+    filenamesEn = glob.glob("./en/*/")
+    filenamesSr = glob.glob("./rs/*/")
 
-with open("./en/en-meta.json", 'r+') as file:
-    metaEn = json.load(file)
+    with open("./en/en-meta.json", 'r+') as file:
+        metaEn = json.load(file)
 
-with open("./rs/rs-meta.json", 'r+') as file:
-    metaRs = json.load(file)
+    with open("./rs/rs-meta.json", 'r+') as file:
+        metaRs = json.load(file)
 
-allFilesEn = []
-allFilesRs = []
+    allFilesEn = []
+    allFilesRs = []
 
-for x in filenamesEn:
-    print(x)
+    for x in filenamesEn:
+        allFilesEn.append(x.split("/")[-2]);
 
-for x in filenamesEn:
-    allFilesEn.append(x.split("/")[-2]);
+    for x in filenamesSr:
+        allFilesRs.append(x.split("/")[-2]);
 
-for x in filenamesSr:
-    allFilesRs.append(x.split("/")[-2]);
+    originalMetaEn = metaEn["pages"]
+    originalMetaRs = metaRs["pages"]
+    metaEn["pages"] = allFilesEn;
+    metaRs["pages"] = allFilesRs;
 
-metaEn["pages"] = allFilesEn;
-metaRs["pages"] = allFilesRs;
+    with open("./en/en-meta.json", 'w') as file:
+        json.dump(metaEn, file, indent=4)
 
-with open("./en/en-meta.json", 'w') as file:
-    json.dump(metaEn, file, indent=4)
+    with open("./rs/rs-meta.json", 'w') as file:
+        json.dump(metaRs, file, indent=4)
 
-with open("./rs/rs-meta.json", 'w') as file:
-    json.dump(metaRs, file, indent=4)
+    if len(originalMetaEn) < len(allFilesEn):
+        return "Created page: \"" + np.setdiff1d(originalMetaEn, allFilesEn)[0] + '"'
+    else if len(originalMetaEn) > len(allFilesEn):
+        return "Deleted page: \"" + np.setdiff1d(originalMetaEn, allFilesEn)[0] + '"'
+        
+    else if len(originalMetaRs) < len(allFilesRs):
+        return "Stvorena stranica: \"" + np.setdiff1d(originalMetaRs, allFilesRs)[0] + '"'
+    else if len(originalMetaRs) > len(allFilesRs):
+        return "Obrisana stranica: \"" + np.setdiff1d(originalMetaRs, allFilesRs)[0] + '"'
